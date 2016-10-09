4849b66017b55156ee9fd63b25fa6902861cba10
Marcelo Mira <hello@marcemira.com>
2016-10-08 22:57:48 -0300

    removed ember-cli-blanket for now, wasn't working :(

--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 /*jshint node:true*/
 module.exports = {
   "framework": "mocha",
-  "test_page": "tests/index.html?coverage=true#hide_passed",
+  "test_page": "tests/index.html?#hide_passed",
   "disable_watching": true,
   "launch_in_dev": [
     "Chrome"
