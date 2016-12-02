9b85bd0680ddb4cf2122e6898605f90e3f1e8691
marcemira <hello@marcemira.com>
2016-12-01 23:50:37 -0300

    trying to fix CI tests

--- testem.js
+++ testem.js
@@ -1,13 +1,16 @@
 /*jshint node:true*/
 module.exports = {
-  "framework": "qunit",
+  "framework": "mocha",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "PhantomJS",
+    "Chrome",
+    "Firefox"
   ],
   "launch_in_dev": [
     "PhantomJS",
-    "Chrome"
+    "Chrome",
+    "Firefox"
   ]
 };
